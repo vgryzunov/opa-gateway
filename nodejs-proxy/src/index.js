85c62580061135c78c32117caf64cc8ab54df75b
vgryzunov <vgryzunov@reinform.ru>
2022-04-14 10:43:48 +0300

    nodejs-proxy package.json added to git added route for /api

--- nodejs-proxy/src/index.js
+++ nodejs-proxy/src/index.js
@@ -8,8 +8,10 @@ const app = express();
 // Configuration
 const PORT = 3000;
 const HOST = "localhost";
+
 const KEYCLOAK_SERVER_URL = "http://192.168.49.2:30355";
 const NG_URL = "http://localhost:4200"
+const API_URL = "http://localhost:8888"
 
 app.use(morgan('dev'));
 
@@ -17,7 +19,6 @@ app.get('/info', (req, res, next) => {
     res.send('This is a proxy service which proxies requests for this project.');
 });
 
-// Proxy endpoints
 app.use('/iam', createProxyMiddleware({
     target: KEYCLOAK_SERVER_URL,
     changeOrigin: true,
@@ -35,8 +36,26 @@ app.use('/ng', createProxyMiddleware({
         [`^/ng`]: '/ng',
     },
     secure: false,
+    xfwd: true,
+    ws: true
+}));
+
+app.use('/api', createProxyMiddleware({
+    target: API_URL,
+    changeOrigin: true,
+    pathRewrite: {
+        [`^/api`]: '',
+    },
+    secure: false,
     xfwd: true
 }));
+
+app.use('/oauth2/authorization/keycloak2', createProxyMiddleware({
+    target: API_URL,
+    changeOrigin: true,
+    secure: false,
+    xfwd: true
+}))
 
 // Start the Proxy
 app.listen(PORT, HOST, () => {
